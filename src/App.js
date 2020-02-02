6a019a1c64613a3210195a3c0c29e7181bd218f2
Pumpuli <uqqeli@gmail.com>
2020-02-03 00:16:03 +0200

    Prepare for react-router v6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, NavLink } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 import "./App.css"; 
 import Timer from "./Timer/Timer";
 import Quiz from "./Quiz/Quiz";
@@ -64,19 +64,21 @@ class App extends Component {
         </button>
 
         <div className="page-container">
-          <Route path="/timer" render={() => <Timer targetDate={this.getTargetDate()} title="Time until the Call the Midwife 2020 Christmas Special airs (hopefully):" />} />
-          <Route path="/us-timer" render={() => <Timer targetDate={Date.UTC(2020, 2, 30, 3, 0, 0)} title="Time until Call the Midwife season 9 airs (in the US):"/>} />
-          <Route path="/quiz" component={Quiz} />
-          <Route path="/" component={Home} exact />
-          <Route path="/contact" component={Contact} />
-          <Route path="/episode-randomizer" component={Randomizer} />
-          <Route path="/bingo" component={Bingo} />
-          <Route path="/videos" component={VideoPage} />
-          <Route path="/podcasts" component={PodcastPage} />
-          <Route path="/cookies" component={CookiesPage} />
-          <Route path="/music" component={MusicPage} />
-          <Route path="/subs" component={Subs} />
-          <Route path="/quotes" component={QuotesPage} />
+          <Switch>
+            <Route path="/timer"><Timer targetDate={this.getTargetDate()} title="Time until the Call the Midwife 2020 Christmas Special airs (hopefully):" /></Route>
+            <Route path="/us-timer"><Timer targetDate={Date.UTC(2020, 2, 30, 3, 0, 0)} title="Time until Call the Midwife season 9 airs (in the US):"/></Route>
+            <Route path="/quiz"><Quiz /></Route>
+            <Route path="/" exact><Home /></Route>
+            <Route path="/contact"><Contact /></Route>
+            <Route path="/episode-randomizer"><Randomizer /></Route>
+            <Route path="/bingo"><Bingo /></Route>
+            <Route path="/videos"><VideoPage /></Route>
+            <Route path="/podcasts"><PodcastPage /></Route>
+            <Route path="/cookies"><CookiesPage /></Route>
+            <Route path="/music"><MusicPage /></Route>
+            <Route path="/subs"><Subs /></Route>
+            <Route path="/quotes"><QuotesPage /></Route>
+          </Switch>
         </div>
 
         <div className="page-footer">
